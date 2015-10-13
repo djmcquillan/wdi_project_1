af5e95866f415b3446ec2e55fc9f3af5db9a13c4
Dan McQuillan <djmcquillan76@gmail.com>
2015-10-13 14:45:35 -0700

    moveForward function using append

--- project1.js
+++ project1.js
@@ -1,20 +1,26 @@
 
 console.log('Script running');
 var dice = $('.dice');
-var boardPosition = document.querySelectorAll('.board-position');
+var boardPosition = $('.board-position');
 var turn = 0;
 var bluePiece = $('[data-id = "blue"]');
 var greenPiece = $('[data-id = "green"]');
-var blueStart = $('[data-id = "slot1"]');
-var greenStart = $('[data-id = "slot15"]');
-var track = $('.track-container');
+// var blueStart = $('[data-id = "0"]');
+// var greenStart = $('[data-id = "14"]');
 var homeBlue = $('.blue-home');
 var homeGreen = $('.green-home');
+var blueMove; 
+var greenMove; 
+
+
+
 
 
 // function created to show roll of dice 1-6 when pressed
 dice.on('click',function(){
 	dice = Math.floor((Math.random()) * 7);
+	blueMove = Number(bluePiece.parent().attr('data-id')) + dice;
+	greenMove = Number(greenPiece.parent().attr('data-id')) + dice;
 	console.log(dice);
 	playerTurn();
 	homeToStart();
@@ -36,42 +42,28 @@ function playerTurn() {
 // move game piece from home position to starting board position
 function homeToStart(){
 		if (dice === 6){
-			console.log('move from home or move player on board');
 			if(turn % 2 === 0){
-				bluePiece.click(function(){
-				$(this).detach();
-					blueStart.append($(this));
-					});
-			}
+				boardPosition.eq(0).append(bluePiece);
+					}
 			else if(turn % 2 !== 0){
-				greenPiece.click(function(){
-				$(this).detach();
-					greenStart.append($(this));
-					});	
+				boardPosition.eq(14).append(greenPiece);
+					}	
 			}
 		}
-	}
 
 function moveForward() {
-	
-	var move = boardPosition[dice];
-	// var nextMove = $(track.nextAll().eq(move));
-		console.log(move);
 			if(homeBlue.innerHTML === ''){
 				if(turn % 2 === 0){
-				bluePiece.click(function(){
-					$(this).detach();
-					// nextMove.append($(this));
-					});
+					bluePiece.parent().attr('data-id') 
+				// boardPosition.eq(blueMove).append(bluePiece);
 				}
 				else if(homeGreen.innerHTML === ''){
-					(turn % 2 !== 0)
-					greenPiece.click(function(){
-					$(this).detach();
-					// nextMove.append($(this));
-					});
+					if(turn % 2 !==0){
+						boardPosition.eq(greenMove).append(greenPiece);
+					}
 				}
 			}
-		}	
+		}
+			
 
 		
